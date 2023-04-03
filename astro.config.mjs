538cc1b28688f5d54f5da1b88673686c7c9cd990
Giammarco <giammarcogaudenzi@gmail.com>
2023-04-03 13:21:22 +0200

    update base url

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   site: "https://giansake.github.io",
-  base: "/effe-elle-eventi/",
+  base: "/EffeElleEventi",
   integrations: [tailwind()],
 });
