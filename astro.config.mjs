9ad1949faa523e055cb24946e0e748132b759dfd
Giammarco <giammarcogaudenzi@gmail.com>
2023-04-03 13:18:23 +0200

    change base url for github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   site: "https://giansake.github.io",
-  base: "/EffeElleEventi",
+  base: "/effe-elle-eventi/",
   integrations: [tailwind()],
 });
