e8df1aa65320484c455666a737a8eae53a4c466f
Giammarco <giammarcogaudenzi@gmail.com>
2023-04-03 17:43:24 +0200

    mobile overlay menu svelte component

---
+++ src/components/OverlayMenu.svelte
@@ -0,0 +1,26 @@
+<script>
+  import { fade } from "svelte/transition";
+  export let isOpen = false;
+</script>
+
+<button
+  class="relative z-[22]"
+  on:click={() => {
+    isOpen = !isOpen;
+  }}
+>
+  {#if !isOpen}
+    <span>open </span>
+  {:else}
+    <span>close</span>
+  {/if}
+</button>
+
+{#if isOpen}
+  <div
+    transition:fade={{ duration: 100 }}
+    class="w-[100vw] h-[100vh] bg-red-800 text-white absolute top-0 left-0 z-[1] flex flex-col items-center justify-center"
+  >
+    <span>overlay menu content</span>
+  </div>
+{/if}
